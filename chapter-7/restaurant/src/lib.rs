77db7e74f2cb6d3fedcc798fff9e318833aa7752
danitrod <danitrod@ibm.com>
2020-09-23 13:53:09 -0300

    Add separate files and importing

--- chapter-7/restaurant/src/lib.rs
+++ chapter-7/restaurant/src/lib.rs
@@ -33,40 +33,79 @@ mod tests {
 //     fn cook_order() {}
 // }
 
-fn _main() {
-    mod back_of_house {
-        pub struct Breakfast {
-            pub toast: String,
-            seasonal_fruit: String,
-        }
+// fn _main() {
+//     mod back_of_house {
+//         pub struct Breakfast {
+//             pub toast: String,
+//             seasonal_fruit: String,
+//         }
 
-        impl Breakfast {
-            pub fn summer(toast: &str) -> Breakfast {
-                Breakfast {
-                    toast: String::from(toast),
-                    seasonal_fruit: String::from("peaches"),
-                }
-            }
-        }
+//         impl Breakfast {
+//             pub fn summer(toast: &str) -> Breakfast {
+//                 Breakfast {
+//                     toast: String::from(toast),
+//                     seasonal_fruit: String::from("peaches"),
+//                 }
+//             }
+//         }
 
-        pub enum Appetizer {
-            Soup,
-            Salad,
-        }
-    }
+//         pub enum Appetizer {
+//             Soup,
+//             Salad,
+//         }
+//     }
 
-    pub fn eat_at_restaurant() {
-        // Order a breakfast in the summer with Rye toast
-        let mut meal = back_of_house::Breakfast::summer("Rye");
-        // Change our mind about what bread we'd like
-        meal.toast = String::from("Wheat");
-        println!("I'd like {} toast please", meal.toast);
+//     pub fn eat_at_restaurant() {
+//         // Order a breakfast in the summer with Rye toast
+//         let mut meal = back_of_house::Breakfast::summer("Rye");
+//         // Change our mind about what bread we'd like
+//         meal.toast = String::from("Wheat");
+//         println!("I'd like {} toast please", meal.toast);
 
-        // The next line won't compile if we uncomment it; we're not allowed
-        // to see or modify the seasonal fruit that comes with the meal
-        // meal.seasonal_fruit = String::from("blueberries");
+//         // The next line won't compile if we uncomment it; we're not allowed
+//         // to see or modify the seasonal fruit that comes with the meal
+//         // meal.seasonal_fruit = String::from("blueberries");
 
-        let _order1 = back_of_house::Appetizer::Soup;
-        let _order2 = back_of_house::Appetizer::Salad;
-    }
+//         let _order1 = back_of_house::Appetizer::Soup;
+//         let _order2 = back_of_house::Appetizer::Salad;
+//     }
+// }
+
+
+// Alias name importing
+// #![allow(unused)]
+// fn main() {
+// use std::fmt::Result;
+// use std::io::Result as IoResult;
+
+// fn function1() -> Result {
+//     // --snip--
+//     Ok(())
+// }
+
+// fn function2() -> IoResult<()> {
+//     // --snip--
+//     Ok(())
+// }
+// }
+
+// Single line multiple imports
+// use std::{cmp::Ordering, io};
+
+// use std::io;
+// use std::io::Write;
+// use std::io::{self, Write};
+
+// Import everything
+// use std::collections::*;
+
+// Using other files
+mod front_of_house; // Imports ./front_of_house.rs
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 }
